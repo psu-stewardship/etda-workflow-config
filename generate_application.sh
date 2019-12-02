be632aa62ded4a949b22041d95553b06c7e3a4c0
DroneCI <drone@drone-test.dsrd.libraries.psu.edu>
2019-12-02 15:20:23 -0500

    hardcode in enabled

--- generate_application.sh
+++ generate_application.sh
@@ -34,6 +34,7 @@ yq w argocd/$branch_slugified.yaml metadata.name etda-workflow-$branch_slugified
 yq w argocd/$branch_slugified.yaml spec.destination.namespace etda-workflow-$branch_slugified -i
 yq w argocd/$branch_slugified.yaml spec.source.helm.values.image.tag $DRONE_BUILD_NUMBER -i 
 yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.path $vault_path -i
+yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.enabled true -i
 yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.role etda-workflow-$config_env -i 
 yq w argocd/$branch_slugified.yaml spec.source.helm.values.fqdn $fqdn -i 
 yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.mountPath $vault_mount_path -i
