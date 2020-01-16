b1184f87f81dfb65b817e0cb2cb7dc41e8be2240
DroneCI <drone@drone-test.dsrd.libraries.psu.edu>
2020-01-15 21:04:07 -0500

    update gen_app

--- generate_application.sh
+++ generate_application.sh
@@ -1,7 +1,5 @@
 domain_name="dsrd.libraries.psu.edu"
 config_env=${CONFIG_ENV-dev}
-set -e
-set -x
 
 ## Configure git
 git config user.email "drone@drone-test.dsrd.libraries.psu.edu"
@@ -16,7 +14,7 @@ if [ $config_env == "prod" ]; then
     vault_mount_path="auth/k8s-dsrd-prod"
     echo "we aren't in prod yet. bailing early"
     exit 0
-    fqdn=scholarsphere.$domain_name
+    fqdn=submit-etda.$domain_name
 else
     vault_mount_path="auth/k8s-dsrd-dev"
     fqdn=submit-etda-$branch_slugified.$domain_name
@@ -24,20 +22,40 @@ fi
 
 vault_login_role="etda-workflow-${CONFIG_ENV:-dev}"
 
+initalize_app=false
 ## copy the template if we need to
 if [ ! -f argocd/$branch_slugified.yaml ]; then 
+    initalize_app=true
     cp template.yaml argocd/$branch_slugified.yaml
 fi
 
-# process the template
-yq w argocd/$branch_slugified.yaml metadata.name etda-workflow-$branch_slugified -i
-yq w argocd/$branch_slugified.yaml spec.destination.namespace etda-workflow-$branch_slugified -i
-yq w argocd/$branch_slugified.yaml spec.source.helm.values.image.tag $DRONE_BUILD_NUMBER -i 
-yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.path $vault_path -i
-yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.enabled true -i
-yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.role etda-workflow-$config_env -i 
-yq w argocd/$branch_slugified.yaml spec.source.helm.values.fqdn $fqdn -i 
-yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.mountPath $vault_mount_path -i
+
+# Turn the block into yaml before proccessing
+sed -i -e 's/^\([[:space:]]*\)values: |/\1values:/g' argocd/$branch_slugified.yaml
+
+function initalize_app {
+    echo "initalizing app"
+    yq w argocd/$branch_slugified.yaml metadata.name etda-workflow-$branch_slugified -i
+    yq w argocd/$branch_slugified.yaml spec.destination.namespace etda-workflow-$branch_slugified -i
+    yq w argocd/$branch_slugified.yaml spec.source.helm.values.image.tag $DRONE_BUILD_NUMBER -i 
+    yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.path $vault_path -i
+    yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.enabled true -i
+    yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.role etda-workflow-$config_env -i 
+    yq w argocd/$branch_slugified.yaml spec.source.helm.values.fqdn $fqdn -i 
+    yq w argocd/$branch_slugified.yaml spec.source.helm.values.global.vault.mountPath $vault_mount_path -i
+}
+
+function update_app {
+    echo "upating app"
+    yq w argocd/$branch_slugified.yaml spec.source.helm.values.image.tag $DRONE_BUILD_NUMBER -i 
+}
+
+## we only update the image tag if the file has been copied.
+if [ "$initalize_app" = true ]; then 
+    initalize_app
+else
+    update_app
+fi
 
 # Turn the values block into a string 
 sed -i -e 's/[[:space:]]values:/values: |/g' argocd/$branch_slugified.yaml
